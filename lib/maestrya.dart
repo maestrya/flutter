9a43d97226a64f50cb0da042e68311ac473e3789
velrino <denismagalhaesvelrino@gmail.com>
2020-02-15 15:27:51 -0300

    :art: implemented the function that will analyze, redirect and render the texts

--- lib/maestrya.dart
+++ lib/maestrya.dart
@@ -1,7 +1,34 @@
 library maestrya;
 
-/// A Calculator.
+import 'package:flutter/material.dart';
+
 class Maestrya {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  List<Widget> handleWidgets(list) {
+    List<Widget> childrenWidgets = [];
+    for (final item in list) {
+      final value = new GenericStringDynamicInferface(item);
+      if (item['type'] == 'text') {
+        childrenWidgets.add(TextWidget(params: value));
+      } 
+    }
+    return childrenWidgets;
+  }
+}
+
+class TextWidget extends StatelessWidget {
+  final GenericStringDynamicInferface params;
+
+  TextWidget({Key key, @required this.params}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Text(this.params.data['value']['text']));
+  }
+}
+
+class GenericStringDynamicInferface {
+  final Map<String, dynamic> data;
+
+  GenericStringDynamicInferface(this.data);
 }

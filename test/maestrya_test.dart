61a170bb46a20cb4f03d29c09d7859c83b3597ca
velrino <denismagalhaesvelrino@gmail.com>
2020-02-15 16:32:12 -0300

    :art: created unit test for only TextWidgets

--- test/maestrya_test.dart
+++ test/maestrya_test.dart
@@ -1,13 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 
 import 'package:maestrya/maestrya.dart';
+import './data.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    dynamic params = data['success']['body']['render'];
+    
+    final value = GenericStringDynamicInferface(params[0]);
+
+    List<Widget> childrenWidgets = [];
+    childrenWidgets.add(TextWidget(params: value));
+
+    final instance = Maestrya();
+    List<Widget> result = instance.handleWidgets([params[0]]);
+
+    expectLater(result, result);
   });
 }

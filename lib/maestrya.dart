e89373073a1d947c95b23d1b74b8473c00bbf49b
velrino <denismagalhaesvelrino@gmail.com>
2020-02-15 16:37:03 -0300

    :art: added DropdownButtonWidgetState

--- lib/maestrya.dart
+++ lib/maestrya.dart
@@ -11,6 +11,8 @@ class Maestrya {
         childrenWidgets.add(TextWidget(params: value));
       } else if (item['type'] == 'input') {
         childrenWidgets.add(InputWidgetState(params: value));
+      } else if (item['type'] == 'select') {
+        childrenWidgets.add(DropdownButtonWidget(params: value));
       }
     }
     return childrenWidgets;
@@ -65,3 +67,48 @@ class InputWidgetState extends StatelessWidget {
   }
 }
 
+
+class DropdownButtonWidget extends StatefulWidget {
+  final GenericStringDynamicInferface params;
+
+  DropdownButtonWidget({Key key, @required this.params}) : super(key: key);
+
+  @override
+  DropdownButtonWidgetState createState() =>
+      DropdownButtonWidgetState(params: null);
+}
+
+class DropdownButtonWidgetState extends State<DropdownButtonWidget> {
+  String dropdownValue = 'One';
+  final GenericStringDynamicInferface params;
+
+  // In the constructor, require a Person
+  DropdownButtonWidgetState({Key key, @required this.params});
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['One', 'Two', 'Free', 'Four']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}

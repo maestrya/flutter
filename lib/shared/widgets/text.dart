ffb7b9aa1cd7667f202f77d77d75169bbc14ee31
velrino <denismagalhaesvelrino@gmail.com>
2020-02-18 02:51:03 -0300

    :zap: added params as style

--- lib/shared/widgets/text.dart
+++ lib/shared/widgets/text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maestrya/shared/resources/index.dart';
 
 class TextWidget extends StatelessWidget {
   final dynamic data;
@@ -6,9 +7,14 @@ class TextWidget extends StatelessWidget {
   TextWidget({Key key, @required this.data}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-
     return Padding(
         padding: const EdgeInsets.symmetric(vertical: 16.0),
-        child: Text(this.data['value']['text']));
+        child: Text(
+          this.data['value']['text'],
+          style: TextStyle(
+             color: HexColor(this.data['textColor']),
+              height: checkDouble(this.data['height']),
+              fontSize: checkDouble(this.data['size'])),
+        ));
   }
 }

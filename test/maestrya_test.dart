d118e45600d5e9f2f8bf3966c1ac7c9bfdb969b5
velrino <denismagalhaesvelrino@gmail.com>
2020-02-18 02:58:30 -0300

    :bug: tdd
    git diff

--- test/maestrya_test.dart
+++ test/maestrya_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-
+import 'package:maestrya/shared/widgets/index.dart';
 import 'package:maestrya/maestrya.dart';
 import './data.dart';
 
@@ -8,13 +8,11 @@ void main() {
   test('success render TextWidget', () {
     dynamic params = data['success']['body']['render'];
 
-    final value = GenericStringDynamicInferface(params[0]);
-
     List<Widget> childrenWidgets = [];
-    childrenWidgets.add(TextWidget(params: value));
+    childrenWidgets.add(TextWidget(data: params));
 
     final instance = Maestrya();
-    List<Widget> result = instance.render([params[0]]);
+    List<Widget> result = instance.render(params);
 
     expect(result, result);
   });
@@ -22,13 +20,11 @@ void main() {
   test('success render InputWidget', () {
     dynamic params = data['success']['body']['render'];
 
-    final value = GenericStringDynamicInferface(params[1]);
-
     List<Widget> childrenWidgets = [];
-    childrenWidgets.add(InputWidget(params: value));
+    childrenWidgets.add(InputWidget(data: params[1]));
 
     final instance = Maestrya();
-    List<Widget> result = instance.render([params[0]]);
+    List<Widget> result = instance.render(params);
 
     expect(result, result);
   });
@@ -36,13 +32,12 @@ void main() {
   test('success render DropdownButtonWidget', () {
     dynamic params = data['success']['body']['render'];
 
-    final value = GenericStringDynamicInferface(params[3]);
 
     List<Widget> childrenWidgets = [];
-    childrenWidgets.add(DropdownButtonWidget(params: value));
+    childrenWidgets.add(DropdownWidget(data: params[3]));
 
     final instance = Maestrya();
-    List<Widget> result = instance.render([params[3]]);
+    List<Widget> result = instance.render(params);
 
     expect(result, result);
   });
@@ -50,13 +45,11 @@ void main() {
   test('success render CardWidget', () {
     dynamic params = data['success']['body']['render'];
 
-    final value = GenericStringDynamicInferface(params[4]);
-
     List<Widget> childrenWidgets = [];
-    childrenWidgets.add(CardWidget(params: value));
+    childrenWidgets.add(CardWidget(data: params[4]));
 
     final instance = Maestrya();
-    List<Widget> result = instance.render([params[4]]);
+    List<Widget> result = instance.render(params);
 
     expect(result, result);
   });

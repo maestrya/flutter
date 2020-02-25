8623e364d2939717c26f83913575ea5c39668bbf
velrino <denismagalhaesvelrino@gmail.com>
2020-02-25 09:59:04 -0300

    :zap: added dynamic_widget

--- lib/maestrya.dart
+++ lib/maestrya.dart
@@ -1,10 +1,23 @@
 library maestrya;
 
 import 'package:flutter/material.dart';
+import 'package:dynamic_widget/dynamic_widget.dart';
 import 'package:maestrya/shared/resources/index.dart';
 import 'package:maestrya/shared/widgets/index.dart';
 
+class DefaultClickListener implements ClickListener{
+  @override
+  void onClicked(String event) {
+    print("Receive click event: " + event);
+  }
+
+}
+
 class Maestrya {
+  final String jsonString;
+
+  Maestrya(this.jsonString);
+  
   render(list) {
     return {
       'body': this.renderBody(list['data']['body']['render']),
@@ -35,5 +48,9 @@ class Maestrya {
       }
     }
     return childrenWidgets;
+  }
+
+  buildWidget(BuildContext context) async {
+    return DynamicWidgetBuilder().build(jsonString, context, new DefaultClickListener());
   }
 }
